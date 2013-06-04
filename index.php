530addd55adbc0df894c3b5cd4e8fcd8799c4959
Barbara Gonzalez <barbyq14@gmail.com>
2013-06-04 11:42:56 -0500

    add som structure

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>CHARMlife</title>
+	<meta charset="utf-8">
+	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<header>
+		<img src="/assets/img/header/logo.png">
+		<nav>
+			<ul>
+				<li><a href="#">HOME</a></li>
+				<li><a href="#">SOCIALES</a></li>
+				<li><a href="#">PERSONALIDADES</a></li>
+				<li><a href="#">+CHARM</a></li>
+				<li><a href="#">PORTADAS</a></li>
+				<li><a href="#">COLABORADORES</a></li>
+				<li><a href="#">CONÓCENOS</a></li>
+			</ul>
+			<ul>
+				<li><a href="#">Anunciate</a></li>
+				<li><a href="#">Suscríbete</a></li>
+			</ul>
+		</nav>
+
+	</header>
+</body>
+</html>
