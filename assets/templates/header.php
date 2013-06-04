c335b4fccdab4b6b21ad6a44f4dedf8d8084d2bf
Juan Carlos Hinojo <jchinojob7@gmail.com>
2013-06-04 12:13:21 -0500

    Smn

---
+++ assets/templates/header.php
@@ -0,0 +1,22 @@
+<div class="main_header">
+	<header class="wrapper">
+		<div class="banners">
+			<img src="/frontend/assets/img/banner1.jpg">
+			<img src="/frontend/assets/img/banner2.png">		
+		</div>
+		<div class="header">
+			<img id="logo" src="/frontend/assets/img/charmlifelogo.png">
+			<div class="navigation">
+					<ul>
+					<li><a href="#home">HOME</a></li>
+					<li><a href="#sociales">SOCIALES</a></li>
+					<li><a href="#personalidades">PERSONALIDADES</a></li>
+					<li><a href="#charm">+CHARM</a></li>
+					<li><a href="#portadas">PORTADAS</a></li>
+					<li><a href="#colaboradores">COLABORADORES</a></li>
+					<li><a href="#conocenos">CONÒCENOS</a></li>
+					</ul>
+			</div>
+		</div>
+	</header>
+</div>
---
+++ portadas.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Portadas</title>
+	<meta charset="utf-8">
+	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<?php include "assets/templates/header.php" ?>
+
+</body>
+</html>
