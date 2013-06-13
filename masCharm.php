8f85cd529268d22f59f6462c27193aaccc9db7c1
Barbara Gonzalez <barbyq14@gmail.com>
2013-06-13 12:53:56 -0500

    commit

---
+++ masCharm.php
@@ -0,0 +1,23 @@
+<?php $dir = '/frontend'; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>CHARMlife</title>
+	<meta charset="utf-8">
+	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="<?php echo $dir; ?>/assets/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $dir; ?>/assets/css/stylesheet.css">
+</head>
+<body>
+	<header>
+	</header>
+	<section class="wrapper masCharm">
+		<section class="grid">
+			<p>You</p>
+		</section>
+		<section class="col3">
+			<p>Too</p>
+		</section>
+	</section><!-- wrapper masCharm -->
+</body>
+</html>
