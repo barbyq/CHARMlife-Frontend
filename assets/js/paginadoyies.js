6d2a0bcf30301f130e611649dc3043d9a16222b0
Juan Carlos <jchinojob7@gmail.com>
2013-09-18 16:34:38 -0500

    Changes

--- assets/js/paginadoyies.js
+++ assets/js/paginadoyies.js
@@ -30,7 +30,6 @@ function  ShowColabInfo(colabid) {
 
 function ShowColabArticles (colabid,interva) {
 	$.post("charmadmin/controllers/colaboradores_controller.php",{receiver:"showarticles",showcolab:colabid,interval:interva},function  (response) {
-			console.log(response);
 			var sais = response.length/4;
 			var contador = 0;
 			$('#articulos-section').empty();
@@ -39,17 +38,14 @@ function ShowColabArticles (colabid,interva) {
     			});
 			for (var i = 0; i < response.length; i++) {
 				var mono = response[i];
-				console.log(mono);
 				var yes = $('#templatearti').html();
 				var temp = Handlebars.compile(yes);
 				$('#articulos-section').append(temp(mono));
 			};
-                //Paginado
 	},'json').fail(function  (e) {
 		console.warn(e);
 	});
 	$.post('charmadmin/controllers/colaboradores_controller.php',{receiver:"size",colab:colabid}, function(data, textStatus, xhr) {
-		console.log(data);
 		var cantidaddepaginas = Math.ceil(data.length/8);
 		$('#anchor-wrapper').empty();
 		var pagini = 0;
@@ -62,30 +58,7 @@ function ShowColabArticles (colabid,interva) {
 			e.preventDefault();
 			var primier = e.target.id;
 			var eighti = parseInt(primier);
-			console.log(eighti)
 			ShowColabArticles(colabid,eighti);
 		});             	
         },'json');
-}
-
-function GetType (blipo) {
-	var comparador = parseInt(blipo);
-	console.log(blipo);
-	console.log(comparador);
-	switch(comparador){
-		case 0:
-			return "articulo.php";
-			break;
-		case 1:
-			return "galeria.php";
-			break;
-		case 2:
-			return "video.php";
-			break;
-		case 4:
-			return "tematica.php";
-			break;
-		default:
-			break;
-	}
 }
